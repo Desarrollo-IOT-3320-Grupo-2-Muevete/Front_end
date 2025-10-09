5614d144a1d9f2b8bb8d3063685c35142199f88d
Mathias Hidalgo <u202213222@upc.edu.pe>
2025-10-09 17:19:12 -0500

    feat: new front end

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,7 +5,7 @@
 //control.
 
 export const environment = {
-  production: true,
-  serverBasePath: 'https://app-250421124000.azurewebsites.net/api/v1',
+  production: false,
+  serverBasePath: 'http://localhost:8082/api/v1',
   keyGoogle:'AIzaSyDMV3i8LKs4cJYqnT0I_hwv-mw_u55x4VE'
 };
